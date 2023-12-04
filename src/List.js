c41ad0e402fe0b58e89e18dad95cb5661c6a8e77
Khoi Le <lethekhoi@gmail.com>
2023-12-04 09:19:43 +0700

    update search

--- src/List.js
+++ src/List.js
@@ -11,9 +11,11 @@ const App = () => {
   const url = "http://localhost:8080/schedules";
   const urlDelete = "http://localhost:8080/admin/schedules";
   const [contacts, setContacts] = useState([]);
+  const [timeFrame, setTimeFrameValue] = useState('all');
   const token = localStorage.getItem('jsonwebtoken')
   console.log("token", token);
   const navigate = useNavigate();
+  const [courseName, setCourseName] = useState();
 
 
   const fetchInfo = async () => {
@@ -33,18 +35,25 @@ const App = () => {
     event.preventDefault();
     navigate("/addSchedule");
   };
-
+  const handleSearch = (event) => {
+    event.preventDefault();
+     
+   console.log("courseName", courseName)
+   console.log("timeFrame", timeFrame)
+   console.log("search")
+  };
   const handleEditClick = (event, schedule) => {
     event.preventDefault();
     setEditContactId(schedule.id);
     navigate("/editSchedule", {
-       state: { 
-        id: schedule.id, 
-        trainingType: schedule.trainingType, 
-        classType: schedule.classType ,
-        details:schedule.listDetails[0]
-        
-      } });
+      state: {
+        id: schedule.id,
+        trainingType: schedule.trainingType,
+        classType: schedule.classType,
+        details: schedule.listDetails[0]
+
+      }
+    });
 
   };
 
@@ -61,14 +70,45 @@ const App = () => {
     console.log("token delete:", response);
     fetchInfo()
   };
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+    const fieldValue = event.target.value;
+    setCourseName(fieldValue);
+  };
 
   return (
 
     <div>
-      <Header1/>
-      <div className="App">
-      <h1>Course Management System</h1>
-       <br/>
+      <Header1 />
+      <div >
+        <h1 className="float-center">Course Management System</h1>
+       
+        <container className="search">
+
+          <input className="search-item"
+            type="text"
+            defaultValue={""}
+            name="courseName"
+            placeholder="Course Name"
+            onChange={handleAddFormChange}
+          />
+          
+            <select className="search-item"  onChange={event => setTimeFrameValue(event.target.value)}
+              defaultValue={timeFrame}>
+              <option value="all">All</option>
+              <option value="nextWeek">Next Week</option>
+              <option value="nextMonth">Next Month</option>
+              <option value="nextYear">Next Year</option>
+              <option value="lastWeek">Last Week</option>
+              <option value="lastMonth">Last Month</option>
+              <option value="lastYear">Last Year</option>
+            </select>
+           
+          <button className="search-item" onClick={handleSearch}>Search</button>
+        </container >
+        <br />
+
+        <div>
           <table>
             <thead>
               <tr>
@@ -99,11 +139,13 @@ const App = () => {
               ))}
             </tbody>
           </table>
-     
+        </div>
 
-        <form onSubmit={handleAddFormSubmit}>
+        <div className="float-center" onSubmit={handleAddFormSubmit}>
           <button type="submit">New Course</button>
-        </form>
+        </div>
+
+
       </div>
     </div>
   );
